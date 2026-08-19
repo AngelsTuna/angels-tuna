44ba2dea5246c16e8b49296da813d605f71af576
AngelsTuna <angelofato87@gmail.com>
2026-08-20 01:32:08 +0200

    Add files via upload

--- service-worker.js
+++ service-worker.js
@@ -1,4 +1,4 @@
-const CACHE='angels-tuna-v29.4-github-full-1';
+const CACHE='angels-tuna-v29.5-mobile-fishing-time';
 const ASSETS=["./ISTRUZIONI_PC_CORRETTE.txt", "./underwater-tuna-v25.png", "./_redirects", "./icon-192.png", "./underwater-tuna-v29.png", "./iccat-bft-adriatic-events-v29.csv", "./icon-512.png", "./README.md", "./CLOUDFLARE-ISTRUZIONI.txt", "./intro-v20.jpg", "./underwater-tuna-v26.png", "./iccat-bft-adriatic-events-v29.json", "./ISTRUZIONI_PC.txt", "./ocean-background.png", "./index.html", "./manifest.webmanifest", "./intro-video.mp4", "./_headers", "./SCIENTIFIC_MODEL_NOTES.md", "./SERVER_ANGELS_TUNA.ps1"];
 self.addEventListener('install',event=>{
   event.waitUntil(caches.open(CACHE).then(cache=>cache.addAll(ASSETS)).then(()=>self.skipWaiting()));
