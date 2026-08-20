f7f6831b44bcf2acc28666373f812e144e4ddc8a
AngelsTuna <angelofato87@gmail.com>
2026-08-20 02:00:37 +0200

    Add files via upload

--- service-worker.js
+++ service-worker.js
@@ -1,5 +1,6 @@
-const CACHE='angels-tuna-v30.2-final-1';
-const ASSETS=["./ISTRUZIONI_PC_CORRETTE.txt", "./underwater-tuna-v25.png", "./_redirects", "./icon-192.png", "./underwater-tuna-v29.png", "./iccat-bft-adriatic-events-v29.csv", "./icon-512.png", "./README.md", "./CLOUDFLARE-ISTRUZIONI.txt", "./intro-v20.jpg", "./underwater-tuna-v26.png", "./iccat-bft-adriatic-events-v29.json", "./ISTRUZIONI_PC.txt", "./ocean-background.png", "./index.html", "./manifest.webmanifest", "./intro-video.mp4", "./_headers", "./SCIENTIFIC_MODEL_NOTES.md", "./SERVER_ANGELS_TUNA.ps1"];
+const CACHE='angels-tuna-v30.5-reel-sound-1';
+const ASSETS=["./ISTRUZIONI_PC_CORRETTE.txt", "./underwater-tuna-v25.png", "./_redirects", "./icon-192.png", "./underwater-tuna-v29.png", "./iccat-bft-adriatic-events-v29.csv", "./icon-512.png", "./README.md", "./CLOUDFLARE-ISTRUZIONI.txt", "./intro-v20.jpg", "./underwater-tuna-v26.png", "./iccat-bft-adriatic-events-v29.json", "./ISTRUZIONI_PC.txt", "./ocean-background.png", "./index.html",
+  "./reel-strike-v305.wav", "./manifest.webmanifest", "./intro-video.mp4", "./_headers", "./SCIENTIFIC_MODEL_NOTES.md", "./SERVER_ANGELS_TUNA.ps1"];
 self.addEventListener('install',event=>{
   event.waitUntil(caches.open(CACHE).then(cache=>cache.addAll(ASSETS)).then(()=>self.skipWaiting()));
 });
